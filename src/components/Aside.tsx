d52306d444c2dbc6929faa1c7f19735ff3591b49
Prateek Soni <prateek_soni@outlook.com>
2022-09-13 02:35:42 +0530

    Add aside:
    Signed-off-by: Prateek Soni <prateek_soni@outlook.com>

---
+++ src/components/Aside.tsx
@@ -0,0 +1,11 @@
+import { RiChat3Line } from "react-icons/ri";
+
+const Aside = () => {
+  return (
+    <aside className="border-r border-base-100 flex flex-col justify-center items-center">
+      <RiChat3Line className={`w-8 h-8 text-primary`} />
+    </aside>
+  );
+};
+
+export default Aside;
--- src/routes/Dashboard.tsx
+++ src/routes/Dashboard.tsx
@@ -1,5 +1,12 @@
+import { RiChat3Line } from "react-icons/ri";
+import Aside from "../components/Aside";
+
 const Dashboard = () => {
-  return <div>ok</div>;
+  return (
+    <div className="grid grid-cols-[80px_400px_auto] h-screen">
+      <Aside />
+    </div>
+  );
 };
 
 export default Dashboard;
